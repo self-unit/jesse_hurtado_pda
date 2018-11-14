8a1c129a63b5959ee19c4221df3799fdb1f0c712
self-unit <self_unit@hotmail.com>
2018-11-14 11:22:19 +0000

    up to week 7

---
+++ Ruby.rb
@@ -0,0 +1,19 @@
+# arrayOfSongs is an array of song titles
+arrayOfSongs = ["faith", "bohemian rhapsody", "man in the mirror", "englishman in new york"]
+
+#printSongs is a function that prints the array of song titles to the console
+def printSongs(arrayOfSongs)
+    puts arrayOfSongs
+end
+
+# printSongs(arrayOfSongs)
+
+# listOfArtists is a hash of musicians and their respective genres
+listOfArtists = { "pop" => "michael jackson", "rock" => "iggy pop", "rap" => "2pac"}
+
+# printArtists is a function that prints the hash of artists and genres to the console
+def printArtists(listOfArtists)
+    puts listOfArtists
+end
+
+# printArtists(listOfArtists)
