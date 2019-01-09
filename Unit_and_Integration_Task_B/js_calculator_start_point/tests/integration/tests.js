ca669b052967ce0b3422f589322105324b79226e
Jesse <self_unit@hotmail.com>
2019-01-09 23:31:10 +0000

    fixed integration test

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -45,7 +45,8 @@ describe('calculator functionality', function() {
 
   it('should have a working divide button which updates the total', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number48')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number8')).click();
     element(by.css('#operator_divide')).click();
     element(by.css('#number2')).click();
     element(by.css('#operator_equals')).click();
@@ -63,7 +64,7 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('3')
   });
 
-  it('should have produce a negative number when subtracting a larger number from a smaller number', function(){
+  it('should have output of a negative number when subtracting a larger number from a smaller number', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number3')).click();
     element(by.css('#operator_subtract')).click();
@@ -72,27 +73,39 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-6')
   });
 
-  it('should have produce a decimal number when dividing an odd number by an even number', function(){
+  it('should have output of a decimal number when dividing an odd number by an even number', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number5')).click();
     element(by.css('#operator_divide')).click();
     element(by.css('#number2')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('1.5')
+    expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
   });
 
-  it('should have produce a number with 12 digits when adding two 11 digit numbers together', function(){
+  it('should have output of a number with 7 digits when adding a 7 digit number with a 6 digit number', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number18237459628')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#number5')).click();
     element(by.css('#operator_add')).click();
-    element(by.css('#number98273965723')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#number9')).click();
     element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('116511425351')
+    expect(running_total.getAttribute('value')).to.eventually.equal('2806484')
   });
 
   it('should not be able to divide by zero', function(){
     running_total = element(by.css('#running_total'))
-    element(by.css('#number20000')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
     element(by.css('#operator_divide')).click();
     element(by.css('#number0')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('divide by zero!')
